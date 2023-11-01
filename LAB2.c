32001431f5ebe8edad9460d34d25c6471f7f3ee2
Hydra-Bolt <amubashir200018@gmail.com>
2023-11-01 08:17:53 +0500

    lab2

--- LAB2.c
+++ LAB2.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <string.h>
 
-
 // Question 1:
 
 // int main()
@@ -34,7 +33,6 @@
 //     }
 //     while (i<nums);
 
-    
 // }
 
 // Question 2:
@@ -61,7 +59,7 @@
 // }
 
 // Question 3:
-// custom function for string length uses the 
+// custom function for string length uses the
 // int stringLength(const char *str) {
 //     int length = 0;
 
@@ -102,7 +100,7 @@
 //     {
 //         printf("No similarity in the strings");
 //     }
-//     else 
+//     else
 //     {
 //         printf("Number of similar characters: %d", count);
 //     }
@@ -110,12 +108,12 @@
 
 // }
 
-// Question 4: 
+// Question 4:
 // int main() {
 //     char sentence[1000];
 //     printf("Enter a sentence: ");
 //     scanf("%s", sentence);
-    
+
 //     for (int i = 0; sentence[i] != '\0'; i++) {
 //         if (sentence[i] >= 'a' && sentence[i] <= 'z') {
 //             sentence[i] = sentence[i] - 'a' + 'A';
@@ -131,19 +129,78 @@
 
 // Question 5:
 
-int main()
+// int main()
+// {
+//     int array[] = {1, 2, 3, 2, 4, 5, 7, 7, 6, 7, 8};
+//     int length = sizeof(array) / sizeof(array[0]);
+//     int uniques[length];
+//     int unique_count = 0;
+//     for (int i = 0; i < length; i++)
+//     {
+//         int isUnique = 1;
+//         for (int j = 0; j < unique_count; j++)
+//         {
+//             if (array[i] == uniques[j])
+//             {
+//                 isUnique = 0;
+//                 break;
+//             }
+//         }
+//         if (isUnique)
+//         {
+//             uniques[unique_count] = array[i];
+//             unique_count++;
+//         }
+//     }
+//     printf("Unique elements in the array: ");
+//     for (int i = 0; i < unique_count; i++)
+//     {
+//         printf("%d ", uniques[i]);
+//     }
+
+//     printf("\n");
+
+//     return 0;
+// }
+
+struct Distance
 {
-    int array[] = {1,2,3,2,4,5,7,7,6,7,8};
+    int feet;
+    int inches;
+};
+struct Distance addDistances(struct Distance d1, struct Distance d2)
+{
+    struct Distance ret;
+    ret.feet = d1.feet + d2.feet;
+    ret.inches = d1.inches + d2.inches;
 
-    for (int i = 0; i<sizeof(array)/sizeof(array[0]); i++)
+    if (ret.inches>=12)
     {
-        for (int j = 0; j<i; j++)
-        {
-            if(array[i] == array[j])
-            {
-                printf("%d is a recurring number in the sequence.\n", array[i]);
-            }
-        }
+        ret.feet++;
+        ret.inches-=12;
     }
+    return ret;
+}
 
+int main()
+
+{
+    struct Distance d1;
+    printf("Enter feet: ");
+    scanf("%d", &d1.feet);
+    printf("Enter inches: ");
+    scanf("%d", &d1.inches);
+
+    printf("\n");
+
+    struct Distance d2;
+    printf("Enter feet: ");
+    scanf("%d", &d2.feet);
+    printf("Enter inches: ");
+    scanf("%d", &d2.inches);
+
+    struct Distance addition = addDistances(d1, d2);
+    printf("%d\'%d\" + %d\'%d\" = %d\'%d\"", d1.feet, d1.inches, d2.feet, d2.inches, addition.feet, addition.inches);
+    return 0;
+    
 }
